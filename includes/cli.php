humanmade/hm-redirects

commit 4fe5c4c7d2eaac5bbcdb7e5c9558f21b85ff8168

Splits `insert_redirect()`'s parameter from an array into seperate variables.
To allow typehinting when this plugin requires PHP 7.

includes/cli.php
@@ -99,8 +99,7 @@ class Commands extends WP_CLI_Command {
 	 * @param string[] $assoc_args Not used.
 	 */
 	public function insert_redirect( $args, $assoc_args ) {
-		$from        = $args[0];
-		$status_code = 301;
+		$from = $args[0];
 
 		if ( ctype_digit( $args[1] ) ) {
 			$to = get_permalink( (int) $args[1] );
@@ -112,7 +111,7 @@ class Commands extends WP_CLI_Command {
 			$to = esc_url_raw( $args[1] );
 		}
 
-		$redirect = Utilities\insert_redirect( compact( 'from', 'to', 'status_code' ) );
+		$redirect = Utilities\insert_redirect( $from, $to, 301 ) );
 
 		if ( is_wp_error( $redirect ) ) {
 			WP_CLI::error( sprintf(
@@ -202,11 +201,11 @@ class Commands extends WP_CLI_Command {
 				WP_CLI::line( "Processing row $row" );
 			}
 
-			$redirect = Utilities\insert_redirect( [
-				'from'        => $redirect_from,
-				'to'          => $redirect_to,
-				'status_code' => 301,
-			] );
+			$redirect = Utilities\insert_redirect(
+				$redirect_from,
+				esc_url_raw( $redirect_to ),
+				301
+			);
 
 			// Record any error notices.
 			if ( is_wp_error( $redirect ) ) {
@@ -370,11 +369,11 @@ class Commands extends WP_CLI_Command {
 
 				// Add redirects.
 				if ( $dry_run === false ) {
-					$redirect = Utilities\insert_redirect( [
-						'from'        => $redirect_from,
-						'to'          => $redirect_to,
-						'status_code' => 301,
-					] );
+					$redirect = Utilities\insert_redirect(
+						$redirect_from,
+						$redirect_to,
+						301
+					);
 
 					// Record any error notices.
 					if ( ! $redirect ) {

includes/utilities.php
@@ -89,22 +89,21 @@ function prefix_path( $url ) {
 /**
  * Add a redirect.
  *
- * @param array $redirect Arguments for the redirect.
+ * @param string $redirect $from Leading-slashed relative URL to redirect away from.
+ * @param string $to Absolute URL to redirect to.
+ * @param int $status_code HTTP status code for the redirect.
+ * @param int $post_id Optional. If set, update that existing redirect.
  *
  * @return int|\WP_Error The post ID if redirect added, otherwise WP_Error on failure.
  */
-function insert_redirect( $redirect ) {
+function insert_redirect( $from, $to, $status_code, $post_id = 0 ) {
 	// Stop loops.
 	remove_action( 'save_post', 'HM\Redirects\\Admin_UI\\handle_redirect_saving', 13 );
-
-	if ( ! isset( $redirect['post_id'] ) ) {
-		$redirect['post_id'] = 0;
-	}
 
 	$result = wp_insert_post(
 		[
 			'ID'                    => $redirect['post_id'],
-			'post_content_filtered' => strtolower( $redirect['status_code'] ),
+			'post_content_filtered' => $redirect['status_code'],
 			'post_excerpt'          => strtolower( $redirect['to'] ),
 			'post_name'             => get_url_hash( $redirect['from'] ),
 			'post_status'           => 'publish',

includes/admin.php
@@ -96,7 +96,7 @@ function handle_redirect_saving( $post_id ) {
 	$to          = isset( $_POST['hm_redirects_to_url'] ) ? sanitize_text_field( wp_unslash( $_POST['hm_redirects_to_url'] ) ) : '';
 	$status_code = isset( $_POST['hm_redirects_status_code'] ) ? sanitize_text_field( wp_unslash( $_POST['hm_redirects_status_code'] ) ) : 302;
 
-	$redirect_id = Utilities\insert_redirect( compact( 'post_id', 'from', 'to', 'status_code' ) );
+	$redirect_id = Utilities\insert_redirect( $from, $to, $status_code, $post_id );
 
 	return $redirect_id === $post_id;
 }

commit 200a8808c03abd345d54b2d495209eb6e2d72370

Fix typos introduced in 4fe5c4c

includes/cli.php
@@ -111,7 +111,7 @@ class Commands extends WP_CLI_Command {
 			$to = esc_url_raw( $args[1] );
 		}
 
-		$redirect = Utilities\insert_redirect( $from, $to, 301 ) );
+		$redirect = Utilities\insert_redirect( $from, $to, 301 );
 
 		if ( is_wp_error( $redirect ) ) {
 			WP_CLI::error( sprintf(

includes/utilities.php
@@ -102,12 +102,12 @@ function insert_redirect( $from, $to, $status_code, $post_id = 0 ) {
 
 	$result = wp_insert_post(
 		[
-			'ID'                    => $redirect['post_id'],
-			'post_content_filtered' => $redirect['status_code'],
-			'post_excerpt'          => strtolower( $redirect['to'] ),
-			'post_name'             => get_url_hash( $redirect['from'] ),
+			'ID'                    => $post_id,
+			'post_content_filtered' => $status_code,
+			'post_excerpt'          => strtolower( $to ),
+			'post_name'             => get_url_hash( $from ),
 			'post_status'           => 'publish',
-			'post_title'            => strtolower( $redirect['from'] ),
+			'post_title'            => strtolower( $from ),
 			'post_type'             => REDIRECTS_POST_TYPE,
 		],
 		true
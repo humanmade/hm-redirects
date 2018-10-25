8326d8496c012f49f897439d2191d68f272b1732
Shantanu <shantanu2846@gmail.com>
2018-10-25 22:18:50 +0530

    Fix class name in arguements to add_command

--- hm-redirects.php
+++ hm-redirects.php
@@ -28,5 +28,5 @@ require_once __DIR__ . '/includes/utilities.php';
 
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	require_once( __DIR__ . '/includes/class-cli-commands.php' );
-	WP_CLI::add_command( 'hm-redirects', 'HM\\Redirects\\CLI\\Commands' );
+	WP_CLI::add_command( 'hm-redirects', 'HM\\Redirects\\CLI\\CLI_Commands' );
 }

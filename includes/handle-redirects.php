humanmade/hm-redirects

commit 23d23cf6b3886a5c1b57301bf42fc5e9e45cc509

use trailingslashit

includes/utilities.php
@@ -49,15 +49,13 @@ function sanitise_and_normalise_url( $unsafe_url ) {
 
 	// Preserve trailing slash if it exists.
 	$has_trailing_slash = substr( $unsafe_url, -1 ) === '/';
-	$unsafe_url = untrailingslashit( $unsafe_url );
-
-	// If the input URL had a trailing slash, we preserve it.
 	if ( $has_trailing_slash ) {
-		$unsafe_url .= '/';
+		// If the input URL has 1+ trailing slashes, we preserve exactly one.
+		$unsafe_url = trailingslashit( $unsafe_url );
 	}
+
 	// Strip query string, hash location and trailing slash.
 	$unsafe_url = strtok( $unsafe_url, '?#' );
-
 
 	// We now can safely escape the URL.
 	$url = esc_url_raw( $unsafe_url );

commit 4dd0b3adef76a4db64e9b9ebe2f014940918fc33

- Add a trailing-slash for local destination URL to avoid "double-redirection" scenario, fix #74 - revert 4a6431e and 23d23cf which caused a regression where trailing-slash URL having a query-string were missed

includes/handle-redirects.php
@@ -86,6 +86,11 @@ function get_redirect_uri( $url ) {
 		return false;
 	}
 
+	if ($to_url[0] === '/') {
+		// It's a local URL. WordPress
+		$to_url = trailingslashit($to_url);
+	}
+
 	// If the URL is only a path, prefix it with the `home_url()`.
 	$to_url = Utilities\prefix_path( $to_url );
 

includes/utilities.php
@@ -47,15 +47,9 @@ function sanitise_and_normalise_url( $unsafe_url ) {
 		$unsafe_url = add_leading_slash( $unsafe_url );
 	}
 
-	// Preserve trailing slash if it exists.
-	$has_trailing_slash = substr( $unsafe_url, -1 ) === '/';
-	if ( $has_trailing_slash ) {
-		// If the input URL has 1+ trailing slashes, we preserve exactly one.
-		$unsafe_url = trailingslashit( $unsafe_url );
-	}
-
 	// Strip query string, hash location and trailing slash.
 	$unsafe_url = strtok( $unsafe_url, '?#' );
+	$unsafe_url = rtrim( $unsafe_url, '/' );
 
 	// We now can safely escape the URL.
 	$url = esc_url_raw( $unsafe_url );